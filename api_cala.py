746f4f353b70ee1e681b44bd8dd6df059b42515e
MichaelOlmosTrujilloACH <miolmos@co.acfspain.org>
2022-05-29 20:03:10 -0500

    proving if changing the url https://api-cala.herokuapp.com/ in api_cala deploy on heroku

--- api_cala.py
+++ api_cala.py
@@ -7,7 +7,7 @@ Created on Sun May 29 07:54:37 2022
 from fastapi import FastAPI
 import uvicorn
 import pandas as pd
-# from analisis import vaders_content
+from analisis import vaders_content
 
 app = FastAPI()
 
@@ -18,7 +18,7 @@ def index():
 @app.get("/items/{q}")
 def read_items(q:int):
     ents = []
-    vaders_content = pd.read_csv('vaders_content.csv', sep = ',')
+    #! vaders_content = pd.read_csv('vaders_content.csv', sep = ',')
     vaders_content_show = vaders_content[:q]
     # print(vaders_content_show[:q])
     for index, row in vaders_content_show.iterrows():
