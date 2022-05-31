a306ad5c6563849bb47f0718de3c599af087bbad
MichaelOlmosTrujilloACH <miolmos@co.acfspain.org>
2022-05-31 09:01:54 -0500

    uploading last changes of analisis.py file and readme.md file

--- analisis.py
+++ analisis.py
@@ -19,53 +19,52 @@ from sklearn import metrics
 plt.style.use('fivethirtyeight')
 
 df_mi_claro = pd.read_csv('df_mi_claro.csv', sep=',')
-# print(df_mi_claro.head())
-# print(df_mi_claro.columns)
-# print(df_mi_claro['content'].head())
-# print(df_mi_claro['score'].head())
-# print(df_mi_claro['reviewId'].head())
-# print(df_mi_claro.shape)
-# print(df_mi_claro['score'].value_counts())
-# fig, ax = plt.subplots(figsize=(10, 7))
-# ax.hist(df_mi_claro['score'], bins = 20)
-# ax = df_mi_claro['score'].value_counts()\
-#     .sort_index()\
-#     .plot(kind='bar', title='Count of Reviews by Stars', figsize=(10, 5))
-# ax.set_xlabel('Reviews Stars')
-# plt.show()
+print(df_mi_claro.head())
+print(df_mi_claro.columns)
+print(df_mi_claro['content'].head())
+print(df_mi_claro['score'].head())
+print(df_mi_claro['reviewId'].head())
+print(df_mi_claro.shape)
+print(df_mi_claro['score'].value_counts())
+ax = df_mi_claro['score'].value_counts()\
+    .sort_index()\
+    .plot(kind='bar', title='Count of Reviews by Stars', figsize=(10, 5))
+ax.set_xlabel('Reviews Stars')
+plt.show()
 
 # Basic NLTK
-# example = df_mi_claro['content'][50]
-# example_2 = df_mi_claro['content'][51]
-# tokens = nltk.word_tokenize(example)
-# print(example)
-# print(example_2)
-# print(tokens)
+example = df_mi_claro['content'][50]
+example_2 = df_mi_claro['content'][51]
+tokens = nltk.word_tokenize(example)
+print(example)
+print(example_2)
+print(tokens)
 # Part of Speech tags
-# tagged = nltk.pos_tag(tokens)
-# print(tagged)
+tagged = nltk.pos_tag(tokens)
+print(tagged)
 # As a text
-# entities = nltk.chunk.ne_chunk(tagged)
-# entities.pprint()
+entities = nltk.chunk.ne_chunk(tagged)
+entities.pprint()
 
 # Vader: Valence Aware Dictionary and sEntiment Reasoner
 
 sia = SentimentIntensityAnalyzer()
-# print(sia)
+print(sia)
 
 #Ejemplos
-# example_polarity = sia.polarity_scores(example)
-# print(example_polarity)
-# example_polarity_2 = sia.polarity_scores(example_2)
-# print(example_polarity_2)
+example_polarity = sia.polarity_scores(example)
+print(example_polarity)
+example_polarity_2 = sia.polarity_scores(example_2)
+print(example_polarity_2)
 # Null values in content column
-# print(df_mi_claro.isnull().sum())
-# print(df_mi_claro.shape)
+print(df_mi_claro.isnull().sum())
+print(df_mi_claro.shape)
 # Drop 8 rows where content column is null
 df_mi_claro = df_mi_claro[df_mi_claro['content'].notnull()]
-# print(df_mi_claro.isnull().sum())
-# print(df_mi_claro.shape)
-# Run polarity score on the entire dataset
+print(df_mi_claro.isnull().sum())
+print(df_mi_claro.shape)
+
+#Run polarity score on the entire dataset
 
 # We save the results in a dictionary
 res = {}
@@ -74,40 +73,40 @@ for i, row in df_mi_claro.iterrows():
     myid = row['reviewId']
     res[myid] = sia.polarity_scores(text)  
 vaders = pd.DataFrame(res).T
-# print(vaders.head())    
+print(vaders.head())    
 vaders = vaders.reset_index().rename(columns={'index': 'reviewId'})
-# vaders.head()
-# print(vaders.shape)
+vaders.head()
+print(vaders.shape)
 vaders = vaders.merge(df_mi_claro, how = 'left')
-# vaders.head()
-# print(vaders.columns)
+vaders.head()
+print(vaders.columns)
 vaders_content = vaders[['content', 'neg', 'pos', 'compound']]
 vaders_content.to_csv('vaders_content.csv', sep = ',')
-# vaders_content.head()
+vaders_content.head()
 
 # Plot VADER results
-# ax = sns.barplot(data = vaders, x = 'score', y = 'compound')
-# ax.set_title('Compound Score by Mi Claro Star Review')
-# plt.show()
+ax = sns.barplot(data = vaders, x = 'score', y = 'compound')
+ax.set_title('Compound Score by Mi Claro Star Review')
+plt.show()
 
 # Plots of positive, negative, neutral reviews
-# fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-# sns.barplot(data = vaders, x = 'score', y = 'pos', ax = axs[0])
-# sns.barplot(data = vaders, x = 'score', y = 'neg', ax = axs[1])
-# sns.barplot(data = vaders, x = 'score', y = 'neu', ax = axs[2])
-# axs[0].set_title('Positive')
-# axs[1].set_title('Negative')
-# axs[2].set_title('Neutral')
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+sns.barplot(data = vaders, x = 'score', y = 'pos', ax = axs[0])
+sns.barplot(data = vaders, x = 'score', y = 'neg', ax = axs[1])
+sns.barplot(data = vaders, x = 'score', y = 'neu', ax = axs[2])
+axs[0].set_title('Positive')
+axs[1].set_title('Negative')
+axs[2].set_title('Neutral')
 
-# Classification algorithm
+# Classification algorithm for starts (score)
 classification_starts = vaders[['reviewId', 'neg', 'pos', 'neu', 
                                 'compound' ,'content', 'score']]
-# classification_starts.head()
+classification_starts.head()
 
 X = classification_starts[['neg', 'pos', 'neu', 'compound']]
 y = classification_starts['score']
-# print(X.head())
-# print(y.head())
+print(X.head())
+print(y.head())
 
 # Split the data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3)
--- readme.md
+++ readme.md
@@ -1,9 +1,15 @@
-1. Run file api_cala.py
-2. api_cala.py file would import analisis.py file which contain the model.
-3. The model is a random_forest_classification algorithm
-4. The variable that contain the data required is "vaders_content"
-5. open the url: http://localhost:8000/items/q 
-6. Replace "q" by any positive integer
-7. you would observe the q first rows of contents with its corresponding 
-   positive, negative and neutral values
+1. To check the data from scrapping run file scrapping.py
+2. To check models from steps 3 and 4 run analisis.py file
+3. Data from scrapping was save in df_mi_claro.csv file
+4. Data from step 3 was save in vaders_content.csv file
+5. To check the api run file api_cala.py
+6. Deploy in heroku use the URL: http://api-cala.herokuapp.com/items/20
+   where we could replace the number 20 by any positive integer. This will 
+   show the 20 first comments with their respective positive, negative and 
+   neutral connotation
+7. The model from step 3 use the library nltk for natural language processing
+   to give the positive, negative, neutral and compound values for each 
+   comment
+8. The model for step 4 use a random forest classification algorithm.
+
    
